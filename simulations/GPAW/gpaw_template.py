69c0b3503e523bb22eced4f7fc68e106a0fb1b9a
13sachin <sachins2879@gmail.com>
2021-11-02 10:32:50 +0530

    update of gpaw template

--- simulations/GPAW/gpaw_template.py
+++ simulations/GPAW/gpaw_template.py
@@ -1,9 +1,10 @@
+from gpaw import GPAW
 
-
-class ground_state_template:
-    """This class contains the template  for creating gpaw 
+class gpaw_ground_state:
+    """This class contains the default parameters and the template for creating gpaw 
     scripts for ground state calculations."""
-    
+    default_param = GPAW.default_parameters
+
     gs_template = """
 from ase.io import read, write
 from ase import Atoms
@@ -54,36 +55,41 @@ calc.write('{work_dir}/gs.gpw', mode='all')
 
     """
     
-class rt_tddft_template:
+class rt_lcao_tddft:
     """This class contains the template  for creating gpaw 
     scripts for  real time tddft calculations."""
 
-    td_template = """
-    # Time-propagation calculation
-    from gpaw.lcaotddft import LCAOTDDFT
-    from gpaw.lcaotddft.dipolemomentwriter import DipoleMomentWriter
-    # Read converged ground-state file
-    td_calc = LCAOTDDFT('gs.gpw', txt='tdx.out')
-    # Attach any data recording or analysis tools
-    DipoleMomentWriter(td_calc, 'dm.dat')
-    # Kick
+    user_input = {}
 
-    td_calc.absorption_kick([", str(Ex), ", ", str(Ey), ", ",  str(Ez),"])
-    # Propagate"
-    td_calc.propagate(", str(dt), ", ", str(Nt),")
-    # Save the state for restarting later"
-    td_calc.write('{directory}td.gpw', mode='all')
+    lcao_tddft_template = """ 
+from gpaw.lcaotddft import LCAOTDDFT
+{import_module}
+td_calc = LCAOTDDFT('gs.gpw', txt='tdx.out')
+
+DipoleMomentWriter(td_calc, 'dm.dat')
+{analysis_tools}
+# Kick
+td_calc.absorption_kick([{absorption_kick}])
+# Propagate"
+td_calc.propagate({propagate})
+# Save the state for restarting later"
+td_calc.write('{directory}td.gpw', mode='all')
     """
+    analysis_tools = [
+        'DipoleMomentWriter()',
+        'WaveFunctionWriter()',
+
+    ]
     
-class lr_tddft_template:
+class lr_tddft:
     """This class contains the template  for creating gpaw 
     scripts for  linear response tddft calculations."""
 
+    user_input = {}
 
-class spectrum_cal_template:
-    """This class contains the template  for creating gpaw 
-    scripts for tddft calculations calculation."""
 
-    spectrum_template = """
-    from gpaw.tddft.spectrum import photoabsorption_spectrum
-    photoabsorption_spectrum('dm.dat', 'spec_x.dat', width=0.09, e_min=0.0, e_max=15.0, delta_e=0.05)"""
+
+
+class induced_density:
+    """Contains template to calculate induced density from density matrix."""
+
