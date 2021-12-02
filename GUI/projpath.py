27940f08c1fc0a9c96b6ec8ac9320ba8b6503db0
Mithun M <mithunm@iiserb.ac.in>
2021-12-02 11:33:40 +0530

    refined menubar, update on open excisting project

--- GUI/projpath.py
+++ GUI/projpath.py
@@ -9,7 +9,7 @@ def create_path(projpath,projname):
     try:
         newpath.mkdir(parents=True, exist_ok=False)
     except FileExistsError:
-        messagebox.showinfo("Message", f"project:{newpath} already exists")
+        messagebox.showinfo("Message", f"project:{newpath} already exists, please select option 'Go'")
     else:
         messagebox.showinfo("Message", f"project:{newpath} is created successfully")
 
