d36a6ab9db4e1acf7e47b82a2a8005fd988b1903
humahuti <dihingiahumahuti@gmail.com>
2021-12-05 13:37:12 +0530

    Added spectra calc and processor number,pathlib fixed

--- simulations/GPAW/spectrum.py
+++ simulations/GPAW/spectrum.py
@@ -12,21 +12,21 @@ class spectrum:
         'delta_e':0.05 
         }
 
-    def cal_photoabs_spectrum(user_input):
+    def cal_photoabs_spectrum(self, user_input):
 
         from gpaw.tddft.spectrum import photoabsorption_spectrum
         photoabsorption_spectrum(user_input['moment_file'], user_input['spectrum_file'],
                         folding=user_input['folding'], width=user_input['width'],
                         e_min=user_input['e_min'], e_max=user_input['e_max'], delta_e=user_input['delta_e'])
 
-    def cal_polarizability_spectrum(user_input):
+    def cal_polarizability_spectrum(self, user_input):
 
         from gpaw.tddft.spectrum import polarizability_spectrum
         polarizability_spectrum(user_input['moment_file'], user_input['spectrum_file'],
                         folding=user_input['folding'], width=user_input['width'],
                         e_min=user_input['e_min'], e_max=user_input['e_max'], delta_e=user_input['delta_e'])
 
-    def cal_rotatory_strength_spectrum(user_input):
+    def cal_rotatory_strength_spectrum(self, user_input):
 
         from gpaw.tddft.spectrum import rotatory_strength_spectrum
         rotatory_strength_spectrum(user_input['moment_file'], user_input['spectrum_file'],
