985333fed5d4a835440a4e10e0a7de88ba40a1e6
humahuti <dihingiahumahuti@gmail.com>
2021-12-17 11:54:15 +0530

    Added x, ylabel

--- GUI/spec_plot.py
+++ GUI/spec_plot.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_spectra(axis, filename, imgfile):
+def plot_spectra(axis, filename, imgfile, x, y):
     data_ej = np.loadtxt(filename)
     
     plt.figure(figsize=(8, 6))
@@ -13,8 +13,10 @@ def plot_spectra(axis, filename, imgfile):
     ax.spines['top'].set_visible(False)
     ax.yaxis.set_ticks_position('left')
     ax.xaxis.set_ticks_position('bottom')
-    plt.xlabel('Energy (eV)')
-    plt.ylabel('Photoabsorption (eV$^{-1}$)')
+    plt.xlabel(x)
+    plt.ylabel(y)
+    # plt.xlabel('Energy (eV)')
+    # plt.ylabel('Photoabsorption (eV$^{-1}$)')
     #plt.xlim(0, 4)
     #plt.ylim(ymin=0)
     plt.tight_layout()
