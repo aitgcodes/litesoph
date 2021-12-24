3c03d4899c0c6bb8343dba6a28d90fdb301033aa
humahuti <dihingiahumahuti@gmail.com>
2021-12-24 20:18:13 +0530

    Added unit conversion

--- GUI/spec_plot.py
+++ GUI/spec_plot.py
@@ -3,12 +3,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_spectra(axis, filename, imgfile, x, y):
-    data_ej = np.loadtxt(filename)
-    
+def plot_spectra(axis, filename, imgfile, x, y, conversion=None):
+    data_ej = np.loadtxt(filename)    
     plt.figure(figsize=(8, 6))
     ax = plt.subplot(1, 1, 1)
-    ax.plot(data_ej[:, 0], data_ej[:, axis], 'k')
+    if conversion is not None:
+        ax.plot(data_ej[:, 0]*conversion, data_ej[:, axis], 'k')
+    else:
+       ax.plot(data_ej[:, 0], data_ej[:, axis], 'k')          
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.yaxis.set_ticks_position('left')
@@ -21,3 +23,4 @@ def plot_spectra(axis, filename, imgfile, x, y):
     #plt.ylim(ymin=0)
     plt.tight_layout()
     plt.savefig(imgfile)
+    plt.show()
