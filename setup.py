9f32a4621e7e87338702876bcd694c2273dbace8
13sachin <sachins2879@gmail.com>
2022-01-01 20:55:21 +0530

    user is asked before appending bashrc

--- setup.py
+++ setup.py
@@ -38,8 +38,27 @@ create_default_config(config, sections)
 with open(config_file, 'w+') as configfile:
     config.write(configfile)
 
-with open(bash_file, 'a') as f:
-    f.write("##----<added by litesoph>-----##\n")
-    f.write(f"export PYTHONPATH=$PYTHONPATH:{lsroot}/\n")
-    f.write(f"export PATH=$PATH:{lsroot}/bin/\n")
-    f.write("##----------------##")
+print("##----<added by litesoph>-----##")
+print(f"export PYTHONPATH=$PYTHONPATH:{lsroot}/")
+print(f"export PATH=$PATH:{lsroot}/bin/")
+print("##----------------##")
+
+msg = "Above code will be appended to ~/.bashrc [y/n]:"
+write_bashrc = input(msg)
+
+if write_bashrc == 'n':
+    exit()
+
+while write_bashrc != 'y':
+    print("----Incorrect input-----")
+    print("enter y or n")
+    write_bashrc = input(msg)
+    if write_bashrc == 'n':
+        break  
+
+if write_bashrc == 'y':
+    with open(bash_file, 'a') as f:
+        f.write("##----<added by litesoph>-----##\n")
+        f.write(f"export PYTHONPATH=$PYTHONPATH:{lsroot}/\n")
+        f.write(f"export PATH=$PATH:{lsroot}/bin/\n")
+        f.write("##----------------##")
