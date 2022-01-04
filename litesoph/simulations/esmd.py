6c94f4bf7810ded7da304a755732b579f2363da3
13sachin <sachins2879@gmail.com>
2022-01-04 21:19:19 +0530

    added run function to tasks

--- litesoph/simulations/esmd.py
+++ litesoph/simulations/esmd.py
@@ -1,15 +1,27 @@
+import configparser
 from typing import Any, Dict
+import pathlib
+from configparser import ConfigParser
 from ase.calculators.calculator import InputError
 from litesoph.simulations.engine import EngineStrategy
+from litesoph.utilities.job_submit import SubmitLocal
+
+config_file = pathlib.Path.home() / "lsconfig.ini"
+if config_file.is_file is False:
+    raise FileNotFoundError("lsconfig.ini doesn't exists")
+
+configs = ConfigParser()
+configs.read(config_file)
 
 class GroundState:
     """It takes in the user input dictionary as input. It then decides the engine and converts 
     the user input parameters to engine specific parameters then creates the script file for that
     specific engine."""
 
-    def __init__(self, user_input: Dict[str, Any],filename, engine: EngineStrategy) -> None:
+    def __init__(self, user_input: Dict[str, Any],engine: EngineStrategy, directroy, filename) -> None:
         self.user_input = user_input
         self.engine = engine
+        self.directory = directroy
         self.task = self.engine.get_task_class('ground state',self.user_input)
 
         if self.engine.check_compatability(self.user_input, self.task):
@@ -18,6 +30,11 @@ class GroundState:
             engine.create_script(self.user_input['directory'],filename, self.template)
         else:
             raise InputError("Input parameters not compatable with the engine")
+
+    def run(self, proc):
+        job = SubmitLocal(self.engine, configs, proc)
+        job.create_command()
+        job.run_job(self.directory)
 
 
 class RT_LCAO_TDDFT:
@@ -31,7 +48,12 @@ class RT_LCAO_TDDFT:
         self.template = self.task.format_template()
 
         engine.create_script(self.directory, filename, self.template)
-        
+    
+    def run(self, proc):
+        job = SubmitLocal(self.engine, configs, proc)
+        job.create_command()
+        job.run_job(self.directory)
+
 class LR_TDDFT:
     pass
 
@@ -46,6 +68,11 @@ class Spectrum:
 
         engine.create_script(self.directory, filename, self.template)
 
+    def run(self, proc):
+        job = SubmitLocal(self.engine, configs, proc)
+        job.create_command()
+        job.run_job(self.directory)
+
 class TCM:
     
     def __init__(self,user_input: Dict[str, Any],filename,engine: EngineStrategy, directory) -> None:
@@ -56,5 +83,10 @@ class TCM:
         self.template = self.task.format_template()
 
         engine.create_script(self.directory, filename, self.template)
-        
+
+    def run(self, proc):
+        job = SubmitLocal(self.engine, configs, proc)
+        job.create_command()
+        job.run_job(self.directory)
+  
 
