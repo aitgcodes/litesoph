88ccb91b6026df7d5985508931f88cd79e7d8039
humahuti <dihingiahumahuti@gmail.com>
2022-01-15 14:43:39 +0530

    Replaced loginval w/ inval

--- litesoph/pre_processing/laser_design.py
+++ litesoph/pre_processing/laser_design.py
@@ -25,9 +25,9 @@ def laser_design(strength, inval, tin, fwhm):
     import math
     tin = 0.0 
     
-    loginval = (-1)*(math.log(float(inval)/float(strength)))
+    #loginval = (-1)*(math.log(float(inval)/float(strength)))
     tau_0 = 2.0*math.sqrt(2*math.log(2.0))/float(fwhm)      # in units of femtosecond
-    t0 = float(tin) + math.sqrt(2.0)*tau_0*math.sqrt(loginval)  # in units of femtosecond
+    t0 = float(tin) + math.sqrt(2.0)*tau_0*math.sqrt(inval)  # in units of femtosecond
     
     tau_0 = tau_0*0.2418                              # converted from fms to eV
     #t0 = t0*(1e3)                                     # converted from fms to attosecond
