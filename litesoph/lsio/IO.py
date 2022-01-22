b3a43eedcce0ef2b043c98a79fa548484299ae2d
13sachin <sachins2879@gmail.com>
2022-01-22 12:25:40 +0530

    added InputTypes

--- litesoph/lsio/IO.py
+++ litesoph/lsio/IO.py
@@ -36,22 +36,3 @@ def write2file(directory,filename, template) -> None:
         f.truncate()
         f.seek(0)
         f.write(template)
-
-def write22file(directory,filename, template, para) -> None:
-    """Write template to a file.
-    
-    directroy: str
-        full path of the directory to write to.
-    filename: str
-        name of the file with extension
-    template: str
-        script template which needs to be written to file.
-    """
-    template = template.format(**para)
-    filename = pathlib.Path(directory) / filename
-
-    with open(filename, 'w+') as f:
-
-        f.truncate()
-        f.seek(0)
-        f.write(template)
