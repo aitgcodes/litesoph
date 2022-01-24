605068c8e18789b2690eca38bfe7907e0d67716d
13sachin <sachins2879@gmail.com>
2022-01-24 10:52:26 +0530

    adding ground stat and settings model

--- litesoph/gui/models.py
+++ litesoph/gui/models.py
@@ -1,5 +1,9 @@
 import pathlib
 import os
+import json
+
+from typing import Any, Dict
+from litesoph.simulations.engine import EngineStrategy, EngineGpaw,EngineNwchem,EngineOctopus
 
 class WorkManagerModel:
     
@@ -28,4 +32,80 @@ class WorkManagerModel:
         os.makedirs(project_path)
 
 class SettingsModel:
-    pass
+
+    options ={
+        'validate job' : {'type': 'bool', 'value': True}
+    }
+    
+    def __init__(self) -> None:
+        filename = "ls_settings.json"
+        self.filepath = pathlib.Path.home() / filename
+        self.load()
+
+    def load(self):
+        if not self.filepath.exists():
+            return
+
+        with open(self.filepath, 'r') as f:
+            raw_values = json.load(f)
+
+        for key in self.options:
+            if key in raw_values and 'value' in raw_values[key]:
+                raw_value = raw_values[key]['value']
+                self.options[key]['value'] = raw_value
+    
+    def save(self):
+        with open(self.filepath, 'w') as f:
+            json.dump(self.options, f)
+
+    def set(self, key, value):
+        if (
+            key in self.options and
+            type(value).__name__ == self.options[key]['type']
+        ):
+            self.options[key]['value'] = value
+        else:
+            raise ValueError("Bad key or wrong variable type")
+
+
+class GroundStateModel:
+    
+    filename = "gs"
+
+    def __init__(self) -> None:
+        pass
+    
+    @staticmethod
+    def choose_engine(user_input: Dict[str, Any]) -> EngineStrategy:
+    
+        list_engine = [EngineGpaw(),
+                        EngineOctopus(),
+                        EngineNwchem()]
+        
+        
+        if user_input['engine'] == 'gpaw':
+            return list_engine[0]
+        if user_input['engine'] == 'octopus':
+            return list_engine[1]
+        elif user_input['engine'] == 'nwchem':
+            return list_engine[2]
+        else:
+            raise ValueError('engine not implemented')
+
+class TextVewerModel:
+
+    def __init__(self, filename) -> None:
+        self.filename = filename
+
+    def read_file(self):
+        with open(self.filename, 'r') as f:
+            self.text = f.read()
+        return self.text
+
+    def append_file(self, text):
+        with open(self.filename, 'a') as f:
+            f.write(text)
+
+    def write_file(self, text):
+        with open(self.filename, 'w') as f:
+            f.write(text)
