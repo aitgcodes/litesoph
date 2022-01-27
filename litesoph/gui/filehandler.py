614c2cd41b9071a6b93b40729b6490c1e6f0d3e2
humahuti <dihingiahumahuti@gmail.com>
2022-01-27 10:29:56 +0530

    added nested dict structure/Status

--- litesoph/gui/filehandler.py
+++ litesoph/gui/filehandler.py
@@ -3,48 +3,108 @@ import subprocess
 import pathlib
 import json
 
-
 class Status():
+
+    default_dict={
+        'engine_run':'',
+        'gpaw':{'gs':{  'state': 0,
+                        'param':{},
+                        'inp': 0,
+                        'cal': 0
+                      },   
+                'td':{  'state': 0,
+                        'param':{},
+                        'inp': 0,
+                        'cal': 0
+                     },       
+                        },
+        'octopus':{'gs':{  'state': 0,
+                        'param':{},
+                        'inp': 0,
+                        'cal': 0
+                      },   
+                'td':{  'state': 0,
+                        'param':{},
+                        'inp': 0,
+                        'cal': 0
+                      },       
+                        },
+    
+        'nwchem':{'gs':{  'state': 0,
+                        'param':{},
+                        'inp': 0,
+                        'cal': 0
+                      },   
+                'td':{  'state': 0,
+                        'param':{},
+                        'inp': 0,
+                        'cal': 0
+                     },       
+                        }                                
+    }
+
     def __init__(self, directory) -> None:
+
         self.filepath = pathlib.Path(directory) / "status.json"
         self.status_dict = {}
         if self.filepath.exists():
             self.read_status()
         else:
-            self.status_dict = {
-                'gs_inp': 0,
-                'td_inp': 0,
-                'gs_cal': 0,
-                'td_cal': 0
-            }
+            self.status_dict = self.default_dict
         self.update_status()
 
     def read_status(self):
+        """ reads the status object from json file & updates the status dictionary"""
+
         with open(self.filepath) as f:
             data_dict = json.load(f)
             self.status_dict.update(data_dict)
             
-    def update_status(self, key=None, value=None):
-        if key is None and value is None:
+    def update_status(self, path:str =None, value=None):
+        """ updates the status dictionary and writes to json file
+         if path(string of keys separated by '.') and value are given"""
+
+        if path is None and value is None:
             dict2json(self.status_dict, self.filepath)
         else:
-            self.status_dict[key] = value
-            dict2json(self.status_dict, self.filepath)
-            return(self.status_dict)
+            obj = self.status_dict
+            list = path.split('.')
+            for i in range(len(list)):
+                for key in obj.keys():
+                    if key == list[i]:
+                        if isinstance(obj[key],dict):
+                            obj =obj[key]               
+                        else:
+                            obj[key] = value 
 
-    def check_status(self, key, value):
+            dict2json(self.status_dict, self.filepath)
+
+    def get_status(self,path:str):
+        """returns the value from the nested dictionary 
+        with path(string of keys separated by '.')"""
+
+        self.read_status()
         try:
-            if self.status_dict[key] == value:
+            obj = self.status_dict
+            list = path.split('.')
+            for i in range(len(list)):
+                key = list[i] 
+                obj = obj[key]   
+            return obj
+        except KeyError:
+            raise KeyError("Key not found")  
+
+    def check_status(self, path, value):
+        """ returns boolean value if given path(keys separated by '.') and value match"""
+
+        try:
+            if self.get_value(path) == value:
                 return True
             else:
                 return False
         except KeyError:
-            return False  
+            return False            
 
-    def get_value(self, key):
-        self.read_status()
-        value = self.status_dict[key]
-        return value
 
 class file_check:
     def __init__(self, check_list:list, dir) -> None:
@@ -65,21 +125,15 @@ class file_check:
         return(check)
 
 
-def dict2file(dictname, filename):
-    filepath = pathlib.Path(filename)
-    with open(filepath, 'w') as status_file:
-        status_file.write(json.dumps(dictname))
-
-
 def search_string(directory, filename, string):
     """ Checks if a string is present in the file and returns boolean"""
+
     inf = str(directory) + '/' + str(filename)
     inf = pathlib.Path(inf)
     if string in open(inf).read():
         return True
     else:
         return False
-
 
 def open_file(outpath):
     text_file = filedialog.askopenfilename(
@@ -91,15 +145,13 @@ def open_file(outpath):
     text_file.close()
     out_file.close()
 
-
 def show_message(label_name, message):
-    """
-    Shows a update
-    """
+    """ Shows a update """
+
     label_name['text'] = message
     label_name['foreground'] = 'black'
 
 def dict2json(dictname, filename):
     filepath = pathlib.Path(filename)
     with open(filepath, 'w') as file:
-        json.dump(dictname, file)
+        json.dump(obj=dictname, fp=file, indent= 3)
