054a9b7f56e4f718c920c0fd865a2824efedd5f7
13sachin <sachins2879@gmail.com>
2022-03-09 20:00:55 +0530

    grouped all config related func into config.py

---
+++ litesoph/config.py
@@ -0,0 +1,105 @@
+import os 
+import subprocess
+import pathlib
+from configparser import ConfigParser
+
+
+sections = {
+    'visualization_tools' : ['vmd', 'vesta'],
+    'engine' : ['gpaw','nwchem','octopus'],
+    'programs' : ['python'],
+    'mpi' : ['mpirun'],
+}
+
+lsroot = pathlib.Path.cwd()
+home = pathlib.Path.home() 
+#bash_file = pathlib.Path(home) / ".bashrc"
+config_file = pathlib.Path(home) / "lsconfig.ini"
+
+
+def get_path(name):
+    print("Checking for {}....".format(name))
+    p = subprocess.run(['which', name], capture_output=True, text=True)
+    if p.stdout and p.returncode == 0:
+        print("Found {} in {}".format(name, p.stdout.split()[0]))
+        return p.stdout.split()[0]
+    else:
+        print("Did not find {}".format(name))
+        return None
+    
+
+def create_default_config(config: ConfigParser, sections: dict):
+    for key, valve in sections.items():
+        config.add_section(key)
+        for option in valve:
+            set = get_path(option)
+            if set is not None:
+                config.set(key, option, set)
+            else:
+                config.set(key, f"#{option} =", None)
+
+def write_config():
+    config = ConfigParser(allow_no_value=True)
+    config.add_section('path')
+    config.set('path','lsproject', str(home))
+    print(f"setting lsroot:{str(lsroot)}")
+    config.set('path','lsroot',str(lsroot))
+    create_default_config(config, sections)
+
+    config.set('mpi', '# gpaw_mpi =', None)
+    config.set('mpi', '# octopus_mpi =', None)
+    config.set('mpi', '# nwchem_mpi =', None)
+
+    with open(config_file, 'w+') as configfile:
+        config.write(configfile)
+
+# print("##----<added by litesoph>-----##")
+# print(f"export PYTHONPATH=$PYTHONPATH:{lsroot}/")
+# print(f"export PATH=$PATH:{lsroot}/bin/")
+# print("##----------------##")
+
+# msg = "Above code will be appended to ~/.bashrc [y/n]:"
+# write_bashrc = input(msg)
+
+# if write_bashrc == 'n':
+#     exit()
+
+# while write_bashrc != 'y':
+#     print("----Incorrect input-----")
+#     print("enter y or n")
+#     write_bashrc = input(msg)
+#     if write_bashrc == 'n':
+#         break  
+
+# if write_bashrc == 'y':
+#     with open(bash_file, 'a') as f:
+#         f.write("##----<added by litesoph>-----##\n")
+#         f.write(f"export PYTHONPATH=$PYTHONPATH:{lsroot}/\n")
+#         f.write(f"export PATH=$PATH:{lsroot}/bin/\n")
+#         f.write("##----------------##")
+
+
+def check_config(lsconfig: ConfigParser, name):
+    if name == "lsroot":
+        try:
+            lsroot = pathlib.Path(lsconfig.get("path", "lsroot" ))
+        except:
+            print("Please set lsroot in ~/lsconfig.ini")
+            exit()
+        else:
+            return lsroot
+    if name == "vis":
+        try:
+           vis_tool = list(lsconfig.items("visualization_tools"))[0][1]
+        except:
+            print("Please set path to vmd or vesta in ~/lsconfig.ini and first one will be used")
+        else:
+            return vis_tool
+        # try:
+        #     vmd = lsconfig.get("visualization_tools", "vmd" )
+        # try:
+        #     vmd = lsconfig.get("visualization_tools", "vesta" )
+        # except:
+        #     print("Please set path to vmd in ~/lsconfig.ini")
+        # else:
+        #     return vmd
