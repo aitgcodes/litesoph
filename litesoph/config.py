72cdd2c0f21bee6d8851f6fa171cbbff09989838
13sachin <sachins2879@gmail.com>
2022-03-14 11:10:40 +0530

    removed unused code

--- litesoph/config.py
+++ litesoph/config.py
@@ -1,4 +1,4 @@
-from distutils.command import config
+
 import os 
 import subprocess
 import pathlib
@@ -6,17 +6,18 @@ from configparser import ConfigParser
 
 import litesoph
 
+
+config_file = pathlib.Path.home() / "lsconfig.ini"
+
+lsroot = pathlib.Path(litesoph.__file__).parent.parent
+
+
 sections = {
     'visualization_tools' : ['vmd', 'vesta'],
     'engine' : ['gpaw','nwchem','octopus'],
     'programs' : ['python'],
     'mpi' : ['mpirun'],
 }
-
-lsroot = pathlib.Path(litesoph.__file__).parent.parent
-home = pathlib.Path.home() 
-#bash_file = pathlib.Path(home) / ".bashrc"
-config_file = pathlib.Path(home) / "lsconfig.ini"
 
 
 def get_path(name):
@@ -43,7 +44,7 @@ def create_default_config(config: ConfigParser, sections: dict):
 def write_config():
     config = ConfigParser(allow_no_value=True)
     config.add_section('path')
-    config.set('path','lsproject', str(home))
+    config.set('path','lsproject', str(pathlib.Path.home()))
     print(f"setting lsroot:{str(lsroot)}")
     config.set('path','lsroot',str(lsroot))
     create_default_config(config, sections)
@@ -60,32 +61,6 @@ def write_config():
         raise e
     else:
         print('Done.')
-
-
-# print("##----<added by litesoph>-----##")
-# print(f"export PYTHONPATH=$PYTHONPATH:{lsroot}/")
-# print(f"export PATH=$PATH:{lsroot}/bin/")
-# print("##----------------##")
-
-# msg = "Above code will be appended to ~/.bashrc [y/n]:"
-# write_bashrc = input(msg)
-
-# if write_bashrc == 'n':
-#     exit()
-
-# while write_bashrc != 'y':
-#     print("----Incorrect input-----")
-#     print("enter y or n")
-#     write_bashrc = input(msg)
-#     if write_bashrc == 'n':
-#         break  
-
-# if write_bashrc == 'y':
-#     with open(bash_file, 'a') as f:
-#         f.write("##----<added by litesoph>-----##\n")
-#         f.write(f"export PYTHONPATH=$PYTHONPATH:{lsroot}/\n")
-#         f.write(f"export PATH=$PATH:{lsroot}/bin/\n")
-#         f.write("##----------------##")
 
 
 def check_config(lsconfig: ConfigParser, name):
