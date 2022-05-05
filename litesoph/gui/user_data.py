3dfd0e260a3d3a0d5286a4990298f78837d10948
13sachin <sachins2879@gmail.com>
2022-05-05 12:17:24 +0530

    modified project storing func and added new func to store and read remote cluster profile

--- litesoph/gui/user_data.py
+++ litesoph/gui/user_data.py
@@ -9,10 +9,17 @@ project_list_file = user_data_dir / 'project_list.txt'
 remote_machine_profile = user_data_dir / 'remote_profile.txt'
 
 def update_proj_list(project: Path) -> None:
-    """Added new projects to the list with timestamp. if project exists it updates the timestamp."""
+    """Added new projects to the list with timestamp. if the project exists it updates the timestamp."""
     proj = ''
+    proj_list = []
     cur_time = time.asctime(time.localtime())
-    proj_list = read_proj_list()
+
+    try:
+        with open(project_list_file, 'r') as f:
+            proj_list = f.readlines()
+    except FileNotFoundError:
+        pass
+
 
     if proj_list:
         for proj_item in proj_list:
@@ -25,20 +32,46 @@ def update_proj_list(project: Path) -> None:
     if not proj:
         proj  = project.name + '\t' + str(project) + '\t'+ cur_time + '\n'
 
-
     with open(project_list_file , 'w+') as wf:   
         proj_list.insert(0, proj)
         for line in proj_list:
-            wf.writelines(line)
+            wf.write(line)
 
-def read_proj_list() -> list:
-    """ Reads the project list file and returns list of lines."""
-    lines = []
-    try:
-        with open(project_list_file, 'r') as f:
-            lines = f.readlines()
-    except FileNotFoundError:
-        return lines
-    else:
-        return lines
         
+def update_remote_profile_list(profile: dict) -> None:
+    """It adds new profile to the list. If the profile is already present it's moved to the top. """
+
+    line  = profile['username'] + '\t' + profile['ip'] + '\t'+ profile['remote_path'] + '\n'
+    profile_list = []
+
+    try:
+        with open(remote_machine_profile, 'r') as f:
+            profile_list = f.readlines()
+    except FileNotFoundError:
+        pass
+          
+    if  line in profile_list:
+        profile_list.remove(line)
+        profile_list.insert(0, line)
+    else:
+        profile_list.insert(0, line)
+
+    with open(remote_machine_profile , 'w+') as wf:   
+        for line in profile_list:
+            wf.write(line)
+
+def get_remote_profile() -> dict:
+
+    try:
+        with open(remote_machine_profile, 'r') as f:
+            profile_list = f.readlines()
+    except FileNotFoundError:
+        return
+    else:
+        profile_list = profile_list[0].strip('\n').split('\t')
+        profile = {
+            'username' : profile_list[0],
+            'ip' : profile_list[1],
+            'remote_path' : profile_list[2]
+        } 
+        return profile
