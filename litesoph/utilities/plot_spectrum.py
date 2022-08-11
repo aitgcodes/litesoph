752dae96b4d67566cf8322642b0f7d5d0af9aba7
13sachin <sachins2879@gmail.com>
2022-08-11 17:33:55 +0530

    added xlimit to plot func

--- litesoph/utilities/plot_spectrum.py
+++ litesoph/utilities/plot_spectrum.py
@@ -20,7 +20,7 @@ def plot(fname,image):
     plt.tight_layout()
     plt.savefig('spec.png')
 
-def plot_spectrum(filename,imgfile,row:int, column:int, x:str, y:str):  
+def plot_spectrum(filename,imgfile,row:int, column:int, xlabel:str, ylabel:str, xlimit=(0.0, 30.0)):  
     """ Shows the plot"""
             
     import numpy as np
@@ -33,9 +33,10 @@ def plot_spectrum(filename,imgfile,row:int, column:int, x:str, y:str):
     ax.spines['top'].set_visible(False)
     ax.yaxis.set_ticks_position('left')
     ax.xaxis.set_ticks_position('bottom')
-    plt.xlabel(x)
-    plt.ylabel(y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.tight_layout()
+    plt.xlim(xlimit[0], xlimit[1])
     plt.savefig(imgfile)
     plt.show()      
 
