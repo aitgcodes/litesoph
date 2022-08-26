8e4a08609a0e8455b048c28b903f8daed473074a
humahuti <dihingiahumahuti@gmail.com>
2022-08-26 15:24:09 +0530

    added plot population

--- litesoph/simulations/octopus/octopus_task.py
+++ litesoph/simulations/octopus/octopus_task.py
@@ -80,7 +80,6 @@ class OctopusTask(Task):
         oct_dir = self.project_dir / engine_dir
 
         if self.task_name in self.added_post_processing_tasks:
-            print("here")
             self.task_dir = self.project_dir/engine_dir/self.task_data.get('dir')
             self.create_directory(self.task_dir)
             self.octopus = Octopus(directory=oct_dir)  
@@ -97,6 +96,7 @@ class OctopusTask(Task):
             self.infile = Path(infile).relative_to(engine_dir)
             self.outfile = Path(outfile).relative_to(engine_dir)
 
+            ## update this creating of directory
             indir = oct_dir / self.infile.parent
             outdir = oct_dir /self.outfile.parent
             for dir in [indir, outdir]:
@@ -183,7 +183,7 @@ class OctopusTask(Task):
         self.write_job_script(self.job_script)
 
     def plot(self,**kwargs):
-        from litesoph.utilities.plot_spectrum import plot_spectrum
+        from litesoph.utilities.plot_spectrum import plot_spectrum,plot_multiple_column
 
         if self.task_name == 'spectrum':
             pol =  self.status.get_status('octopus.rt_tddft_delta.param.TDPolarizationDirection')
@@ -211,7 +211,24 @@ class OctopusTask(Task):
             
             if result[cmd]['returncode'] != 0:
                 raise Exception(f"{result[cmd]['error']}")
-                
+            return
+
+        if self.task_name == 'mo_population_correlation':
+            # first check if the file exists already 
+            import numpy as np
+            from litesoph.post_processing.mo_population import create_states_index
+            below_homo = kwargs.get('num_occupied_mo_plot',1)
+            above_lumo = kwargs.get('num_unoccupied_mo_plot',1)
+            population_diff_file = self.task_dir/'population_diff.dat'
+            self.occ = self.octopus.read_info()[0]
+            
+            # time_unit = kwargs.get('time_unit')            
+            column_range = (self.occ-below_homo+1, self.occ+above_lumo)
+            legend_dict = create_states_index(num_below_homo=below_homo, num_above_lumo=above_lumo, homo_index=self.occ)
+            
+            population_data = np.loadtxt(population_diff_file)            
+            plot_multiple_column(population_data, column_list=column_range, column_dict=legend_dict, xlabel='Time (in h_cut/eV)')
+            return        
 
     @staticmethod
     def get_engine_network_job_cmd():
