5dcea066fe8df1d2452d4d7abe342d54dae42316
13sachin <sachins2879@gmail.com>
2022-08-28 01:29:30 +0530

    fixed bug in gpaw job script creation

--- litesoph/gui/gui.py
+++ litesoph/gui/gui.py
@@ -748,11 +748,7 @@ class GUIAPP:
         np = self.job_sub_page.processors.get()
         rpath = self.job_sub_page.rpath.get()
         if rpath:
-            # Remove this "if" after implementing task class in gpaw
-            if self.engine == 'gpaw':
-                b_file =  task.create_job_script(np, remote_path=rpath, remote=True)
-            else:
-                b_file = task.create_job_script(np, remote_path=rpath)
+            b_file = task.create_job_script(np, remote_path=rpath)
         else:
             messagebox.showerror(title="Error", message="Please enter remote path")
             return
