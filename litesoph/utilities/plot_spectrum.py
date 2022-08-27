b68cf26565ce7e0398af48591e6384df9841f9c3
humahuti <dihingiahumahuti@gmail.com>
2022-08-27 15:52:50 +0530

    added plot_population_tracking method

--- litesoph/utilities/plot_spectrum.py
+++ litesoph/utilities/plot_spectrum.py
@@ -53,16 +53,46 @@ def plot_multiple_column(data_array, column_list, row_range=(1,1), **kwargs):
         column_end = column_list[1] 
     xlabel = kwargs.get('xlabel', 'Time (in as)')
     ylabel = kwargs.get('ylabel', 'Change in population')
+    xcolumn = kwargs.get('xcolumn',0)
     xlimit = kwargs.get('xlimit', None)
     column_dict = kwargs.get('column_dict', {1:"HOMO-2", 2:"HOMO-1", 3:"HOMO"})
 
     plt.figure(figsize=(8, 6))
-    for i in range(column_beg, column_end+1):
+    for i in reversed(range(column_beg, column_end+1)):
         legend = column_dict.get(i)
-        plt.plot(data[:,0], data[:,i], label=legend)
+        plt.plot(data[:,xcolumn], data[:,i], label=legend)
         plt.legend()
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     if xlimit:
         plt.xlim(xlimit[0], xlimit[1])
     plt.show()  
+
+def plot_population_tracking(population_file, homo_index:int,**kwargs):
+    """ Arguments: homo_index= column number corresopnding to homo
+                   num_occupied_mo_plot,num_unoccupied_mo_plot,time_unit
+    """
+    import numpy as np
+    from litesoph.post_processing.mo_population import create_states_index
+    from litesoph.utilities.units import au_to_as,au_to_fs
+
+    population_data = np.loadtxt(population_file)
+    (total_rows, total_columns) = population_data.shape
+    default_above_lumo = total_columns-homo_index-1
+    default_below_homo = homo_index
+
+    below_homo_value = kwargs.get('num_occupied_mo_plot', default_below_homo)
+    above_lumo_value = kwargs.get('num_unoccupied_mo_plot', default_above_lumo)
+
+    # Upper limit on (un/)occupied states
+    below_homo = min(below_homo_value, default_below_homo)
+    above_lumo = min(above_lumo_value, default_above_lumo)
+    
+    time_unit = kwargs.get('time_unit', 'atomic')            
+    column_range = (homo_index-below_homo+1,homo_index+above_lumo)
+    legend_dict = create_states_index(num_below_homo=below_homo, num_above_lumo=above_lumo, homo_index=homo_index)
+
+    if time_unit == 'h_cut/eV':
+        population_data[:,0]= population_data[:,0]*27.12*au_to_fs
+
+    plot_multiple_column(population_data, column_list=column_range, column_dict=legend_dict, xcolumn=0, xlabel='Time (in femtosecond)', ylabel='Change in population')
