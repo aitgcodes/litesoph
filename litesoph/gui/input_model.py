5ac3603eadf893c8723e9af39447fcc175e62e10
humahuti <dihingiahumahuti@gmail.com>
2022-10-11 13:19:54 +0530

    modified gs_input_model

--- litesoph/gui/input_model.py
+++ litesoph/gui/input_model.py
@@ -1,64 +1,64 @@
 from litesoph.test.view_gs import InputFrame
 import tkinter as tk
-# from tkinter import ttk
 from tkinter import Tk
 from tkinter.ttk import Spinbox, Checkbutton, Combobox
 
 gs_model ={"xc family": {
-                    "tab":"Basic",
-                    "group": "theory level",
-                    "text": "Exchange Correlation",
-                    "help": None,
-                    "widget": Combobox,
-                    "values": ["LDA","PBE","PBE0","PBEsol","BLYP","B3LYP","CAMY-BLYP","CAMY-B3LYP",
-                                "PBE96","B3LYP","PW91", "BP86", "BP91","BHLYP","M05","M05-2X",
-                                "M06-HF","M08-SO","M011","CAM-B3LYP","LC-BLYP","LC-PBE","LC-wPBE",
-                                "HSE03","HSE06"
-                                ],
-                    "default": 'PBE',
-        },
+                "tab":"Basic",
+                "group": "theory level",
+                "text": "Exchange Correlation",
+                "help": None,
+                "widget": Combobox,
+                "values": ["LDA","PBE","PBE0","PBEsol","BLYP","B3LYP","CAMY-BLYP","CAMY-B3LYP",
+                        "PBE96","B3LYP","PW91", "BP86", "BP91","BHLYP","M05","M05-2X",
+                        "M06-HF","M08-SO","M011","CAM-B3LYP","LC-BLYP","LC-PBE","LC-wPBE",
+                        "HSE03","HSE06"
+                        ],
+                "default": 'PBE'
+                },
         "basis_type": {
-        "tab":"Basic",
-        "group": "theory level",
-        "text": "Basis Type",
-        "help": None,
-        "widget": Combobox,
-        "values": ["nao","fd","pw","gaussian"],
-        },
+                "tab":"Basic",
+                "group": "theory level",
+                "text": "Basis Type",
+                "help": None,
+                "widget": Combobox,
+                "values": ["nao","fd","pw","gaussian"]
+                },
 
         "basis:nao": {
-        "tab":"Basic",
-        "group": "theory level",
-        "text": "Basis Sets",
-        "help": None,
-        "widget": Combobox,
-        "values": ["dzp","sz","dz","szp","pvalence.dz"],
-        "switch": lambda k:k["basis_type"] == "nao"
-        },
+                "tab":"Basic",
+                "group": "theory level",
+                "text": "Basis Sets",
+                "help": None,
+                "widget": Combobox,
+                "values": ["dzp","sz","dz","szp","pvalence.dz"],
+                "switch": lambda k:k["basis_type"] == "nao"
+                },
 
         "basis:gaussian": {
-        "tab":"Basic",
-        "group": "theory level",
-        "text": "Basis Sets",
-        "help": None,
-        "widget": Combobox,
-        "values": ["6-31G","STO-2G","STO-3G",
-                "STO-6G","3-21G","3-21G*","6-31G*",
-                "6-31G**","6-311G","6-311G*","6-311G**",
-                "cc-pVDZ","aug-cc-pvtz"],
-        "switch": lambda k:k["basis_type"] == "gaussian"
-        },
+                "tab":"Basic",
+                "group": "theory level",
+                "text": "Basis Sets",
+                "help": None,
+                "widget": Combobox,
+                "values": ["6-31G","STO-2G","STO-3G",
+                        "STO-6G","3-21G","3-21G*","6-31G*",
+                        "6-31G**","6-311G","6-311G*","6-311G**",
+                        "cc-pVDZ","aug-cc-pvtz"],
+                "switch": lambda k:k["basis_type"] == "gaussian"
+                },
 
         "spacing": {
-        "tab":"Basic",
-        "group": "theory level",
-        "text": "Grid Spacing",
-        "help": None,
-        "widget": tk.Entry,
-        "type": float,
-        "switch": lambda k:k["basis_type"] == "nao" 
-        or k["basis_type"] == "fd" 
-        },
+                "tab":"Basic",
+                "group": "theory level",
+                "text": "Grid Spacing",
+                "help": None,
+                "widget": tk.Entry,
+                "type": float,
+                "default":0.3,
+                "switch": lambda k:k["basis_type"] == "nao" 
+                or k["basis_type"] == "fd" 
+                },
 
         "box shape": {
                 "tab":"Basic",
@@ -73,10 +73,11 @@ gs_model ={"xc family": {
         "vaccum": {
                 "tab":"Basic",
                 "group": "simulation box",
-                "text": "Vacuum",
+                "text": "Vacuum (in angstrom)",
                 "help": None,
                 "widget": tk.Entry,
                 "type": float,
+                "default":6,
                 "switch": lambda k:
                 k["select box"] == False
                 and k["basis_type"] in ["nao","fd","pw"]
@@ -164,10 +165,11 @@ box_dict = {
                 "tab":"Basic",                   
                 "label_grid":{"row":0, "column":0},
                 "widget_grid":{"row":0, "column":1},
-                "text": "X length",
+                "text": "Length in X (in angstrom)",
                 "help": None,
                 "widget": tk.Entry,
                 "type": float,
+                "default":12,
                 "switch": lambda k:
                 k["box shape"] == "parallelepiped"
                 and k["select box"]
@@ -177,10 +179,11 @@ box_dict = {
                 "tab":"Basic",
                 "label_grid":{"row":1, "column":0},
                 "widget_grid":{"row":1, "column":1},
-                "text": "y length",
+                "text": "Length in Y (in angstrom)",
                 "help": None,
                 "widget": tk.Entry,
                 "type": float,
+                "default":12,
                 "switch": lambda k:k["box shape"] == "parallelepiped"
                 and k["select box"] == True
         },
@@ -188,50 +191,27 @@ box_dict = {
                 "tab":"Basic",
                 "label_grid":{"row":2, "column":0},
                 "widget_grid":{"row":2, "column":1},
-                "text": "z length",
+                "text": "Length in Z (in angstrom)",
                 "help": None,
                 "widget": tk.Entry,
                 "type": float,
+                "default":12,
                 "switch": lambda k:k["box shape"] == "parallelepiped"
                 and k["select box"] == True
         },
-        "sphere radius": {
-                "tab":"Basic",
-                "label_grid":{"row":1, "column":0},
-                "widget_grid":{"row":1, "column":1},
-                "text": "Sphere Radius",
-                "help": None,
-                "widget": tk.Entry,
-                "type": float,
-                "switch": lambda k:
-                k["box shape"] == "sphere"
-                and k["select box"]
-                
-        },
-          "min radius": {
-                "tab":"Basic",
-                "label_grid":{"row":1, "column":0},
-                "widget_grid":{"row":1, "column":1},
-                "text": "Min Radius",
-                "help": None,
-                "widget": tk.Entry,
-                "type": float,
-                "switch": lambda k:
-                k["box shape"] == "minimum" 
-                and k["select box"]               
-        },
-
-        "cylinder radius": {
+        "radius": {
                 "tab":"Basic",
                 "label_grid":{"row":0, "column":0},
                 "widget_grid":{"row":0, "column":1},
-                "text": "Cylinder Radius",
+                "text": "Radius (in angstrom)",
                 "help": None,
                 "widget": tk.Entry,
                 "type": float,
+                "default":12,
                 "switch": lambda k:
-                k["box shape"] == "cylinder"
+                k["box shape"] in ["sphere","minimum","cylinder"]
                 and k["select box"]
+                
         },
         "cylinder length": {
                 "tab":"Basic",
@@ -240,12 +220,15 @@ box_dict = {
                 "label_grid":{"row":1, "column":0},
                 "widget_grid":{"row":1, "column":1},
                 "group": "simulation box",
-                "text": "X length",
+                "text": "Length in X (in angstrom)",
                 "help": None,
                 "widget": tk.Entry,
                 "type": float,
+                "default":12,
                 "switch": lambda k:k["box shape"] == "cylinder"
                 and k["select box"] == True
         },
 
 }
+
+
