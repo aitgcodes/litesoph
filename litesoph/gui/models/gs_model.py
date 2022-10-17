96ac2057ff863ed7d9c97c9f411e6750e5b353ce
humahuti <dihingiahumahuti@gmail.com>
2022-10-17 16:14:08 +0530

    modified choose_engine in gs

--- litesoph/gui/models/gs_model.py
+++ litesoph/gui/models/gs_model.py
@@ -1,17 +1,22 @@
 from tkinter import messagebox
 
 def choose_engine(input_param:dict):
-        basis = input_param.get('basis_type')
+        for basis_key in ['basis_type:common','basis_type:extra']:
+                if input_param.get(basis_key) is not None:                        
+                        basis = input_param.get(basis_key)
+        if basis is None:
+                raise KeyError("No match for basis key")
+
         boxshape = input_param.get('box shape')
         if boxshape:
-                if basis == "nao":
+                if basis == "LCAO":
                         if boxshape == "parallelepiped":
                                 engine = "gpaw"
                                 return engine
                         else:
                                 messagebox.showinfo(message="Not Implemented")
                                 return
-                if basis == "fd":
+                if basis == "FD":
                         if boxshape == "parallelepiped":
                                 check = messagebox.askyesno(title = 'Message',message= "The default engine for the input is gpaw, please click 'yes' to proceed with it. If no, octopus will be assigned")
                                 if check is True:
@@ -22,8 +27,11 @@ def choose_engine(input_param:dict):
                                 engine = "octopus"
                         return engine
         else:
-                if basis == "pw":
+                if basis == "PW":
                         engine = "gpaw"
-                elif basis == "gaussian":
+                elif basis == "Gaussian":
                         engine = "nwchem"
                 return engine
+
+def get_inp_summary(gui_input:dict):
+        pass
