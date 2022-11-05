dd74d81d3ebdc71e5a6c3f59fd95cd4b5ea393be
humahuti <dihingiahumahuti@gmail.com>
2022-11-05 10:09:03 +0530

    modifying Oct/gs task data

--- litesoph/gui/engine_views/octopus_views/gs2oct.py
+++ litesoph/gui/engine_views/octopus_views/gs2oct.py
@@ -1,12 +1,13 @@
+import numpy as np
 
 def create_oct_gs_inp(gui_inp:dict):
     import copy
 
     key2key = {
         "XYZCoordinates":"XYZCoordinates",
-        "max itr": "MaximumIter",
-        "energy conv": "ConvEnergy",
-        "density conv": "ConvAbsDens",
+        "max_itr": "MaximumIter",
+        "energy_conv": "ConvEnergy",
+        "density_conv": "ConvAbsDens",
         "smearing":"Smearing",
         "mixing":"Mixing",
         "bands" : "ExtraStates",
@@ -19,9 +20,13 @@ def create_oct_gs_inp(gui_inp:dict):
         "FromScratch": gui_inp.get("FromScratch","yes"),        
     }
 
-    boxshape = gui_inp.get("box shape")
-    select_box = gui_inp.get("select box")
-    xc = gui_inp.get("xc family")
+    boxshape = gui_inp.get("boxshape")
+    box_dim = gui_inp.get("box_dim")
+    if box_dim is not None:
+        select_box = True
+    else:
+        select_box = False
+    xc = gui_inp.get("xc")
     copy_inp = copy.deepcopy(gui_inp)
     
     for i, (key, value) in enumerate(gui_inp.items()):
@@ -31,67 +36,104 @@ def create_oct_gs_inp(gui_inp:dict):
                 [(key2key.get(key), value)]) 
                 _dict.update(_key2key_dict)
             else:
-                sim_box = get_box_dim(_boxshape= boxshape,
-                                     _from_vacuum= not select_box,
-                                    **copy_inp)
-                _dict.update(sim_box)
-                _dict.update(get_xc_pseudo(xc_str=xc)) 
+                if key == "boxshape":
+                    sim_box = get_box_dim(_boxshape= boxshape,
+                                    _from_vacuum= not select_box,
+                                **copy_inp)
+                    _dict.update(sim_box)               
+                if key == "xc":
+                    _dict.update(get_xc_pseudo(xc_str=xc)) 
     return _dict
 
 def get_box_dim(_boxshape:str,_from_vacuum=False, **kwargs):
     if _from_vacuum :
+        try:
+            _geom_file = kwargs.get("XYZCoordinates")
+            _vacuum = kwargs.get('vacuum', 6)
+        except:
+            raise KeyError("Geometry file not found")
+        _cell = get_box_dim_from_vacuum(_geom_file,_vacuum, _boxshape)
+        
         if _boxshape == "parallelepiped":
-            try:
-                _geom_file = kwargs.get("XYZCoordinates")
-                _vacuum = kwargs.get('vacuum', 6)
-            except:
-                raise KeyError("Geometry file not found")
-            _cell = get_box_dim_from_vacuum(_geom_file,_vacuum, _boxshape)
             _sim_box = {
                         "BoxShape":{"name":_boxshape,
-                        "param":{'LSize':[[
-                            str(_cell[0]/2)+'*angstrom',
-                            str(_cell[1]/2)+'*angstrom',
-                            str(_cell[2]/2)+'*angstrom']]}}
+                                    "param":{'LSize':[[
+                                        str(_cell["lx"]/2)+'*angstrom',
+                                        str(_cell["ly"]/2)+'*angstrom',
+                                        str(_cell["lz"]/2)+'*angstrom']]}}
                             }
-            return _sim_box
-        else:
-            # TODO Getting box dimension for ['cylinder','minimum','sphere']
-            print('Not implemented')
+        elif _boxshape == "cylinder":
+            _sim_box = {
+                        "BoxShape":{"name":_boxshape,
+                                    "param":{
+                                        'Xlength':str(_cell["lx"]/2)+'*angstrom',
+                                        'Radius':str(_cell["radius"])+'*angstrom'}
+                                            }}
+        elif _boxshape in ["sphere","minimum"]:
+            _sim_box = {
+                        "BoxShape":{"name":_boxshape,
+                                    "param":{
+                                        'Radius':str(_cell["radius"])+'*angstrom'}
+                                            }}
+        return _sim_box
+
     elif not _from_vacuum:
-        if _boxshape in ['minimum','sphere']:            
+        box_dict = kwargs.get('box_dim', None)
+        assert box_dict is not None
+
+        if _boxshape in ['minimum','sphere']:     
             _sim_box = {
             "BoxShape":{"name":_boxshape,
-                        "param":{'Radius':str(kwargs.get("radius"))+'*angstrom'}}}
+                        "param":{'Radius':str(box_dict.get("radius"))+'*angstrom'}}}
             return _sim_box 
         elif _boxshape == 'cylinder':
             _sim_box = {
             "BoxShape":{"name":_boxshape,
-                        "param":{'Radius':str(kwargs.get("radius"))+'*angstrom',
-                                'Xlength':str(kwargs.get("cylinder length")/2)+'*angstrom'}}}
+                        "param":{'Radius':str(box_dict.get("radius"))+'*angstrom',
+                                'Xlength':str(box_dict.get("cylinder length")/2)+'*angstrom'}}}
             return _sim_box 
         elif _boxshape == 'parallelepiped':
             _sim_box = {
             "BoxShape":{"name":_boxshape,
                         "param":{'LSize':[[
-                                str(kwargs.get("box length_x")/2)+'*angstrom',
-                                str(kwargs.get("box length_y")/2)+'*angstrom',
-                                str(kwargs.get("box length_z")/2)+'*angstrom']]}}} 
+                                str(box_dict.get("box length_x")/2)+'*angstrom',
+                                str(box_dict.get("box length_y")/2)+'*angstrom',
+                                str(box_dict.get("box length_z")/2)+'*angstrom']]}}} 
             return _sim_box                 
 
 def get_box_dim_from_vacuum(geom_file, vacuum:float,box_shape):
     from ase.io import read
     atoms = read(geom_file)
-    atoms.center(vacuum=vacuum)
+    
     if box_shape == "parallelepiped":
+        atoms.center(vacuum=vacuum)
         cell_param = atoms.cell.cellpar()
         lx =round(cell_param[0], 2)
         ly =round(cell_param[1], 2)
         lz =round(cell_param[2], 2)
+        box_dict = {
+            "lx":lx,
+            "ly":ly,
+            "lz":lz
+        }
 
-        return lx,ly,lz
-    else:
-        raise ValueError("Not implemented")
+    elif box_shape == "cylinder":
+        atoms.center(vacuum=vacuum)
+        cell_param = atoms.cell.cellpar()
+        lx =round(cell_param[0], 2)
+        r = max(cell_param[1], cell_param[2])/2
+        box_dict = {"lx":lx,
+                    "radius":round(r, 2)}
+
+    elif box_shape == "sphere":
+        disp = atoms.get_all_distances()
+        radius = np.ndarray.max(disp)/2 + vacuum
+        box_dict = {"radius":round(radius, 2)}
+
+    elif box_shape == "minimum":
+        box_dict = {"radius":vacuum}
+
+    return box_dict
 
 def set_expt_features():
     pass
