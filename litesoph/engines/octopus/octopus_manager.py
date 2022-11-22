18ca1576e3ab46fb0f504126e7fcef72f85e993c
humahuti <dihingiahumahuti@gmail.com>
2022-11-22 13:26:07 +0530

    updated td_default/octopus

--- litesoph/engines/octopus/octopus_manager.py
+++ litesoph/engines/octopus/octopus_manager.py
@@ -4,7 +4,7 @@ from litesoph.common.task import TaskNotImplementedError
 from litesoph.common.task_data import TaskTypes as tt                    
 from litesoph.common.workflows_data import WorkflowTypes as wt     
 from litesoph.common.engine_manager import EngineManager
-from litesoph.engines.octopus.octopus_task import OctopusTask
+from litesoph.engines.octopus.octopus_task import OctopusTask, calc_td_range
 from litesoph.engines.octopus import task_data as td
 
 class OCTOPUSManager(EngineManager):
@@ -21,19 +21,29 @@ class OCTOPUSManager(EngineManager):
         self.check_task(task_info.name)
         return OctopusTask(config, task_info, dependent_tasks)
     
-    def get_default_task_param(self, name):
+    def get_default_task_param(self, name, dependent_tasks: Union[List[TaskInfo], None]):
         task_default_parameter_map = {
             tt.GROUND_STATE: td.get_gs_default_param,
             tt.RT_TDDFT: td.get_rt_tddft_default_param,
             tt.COMPUTE_SPECTRUM: td.get_compute_spec_param,
+            tt.TCM: td.get_tcm_param,
         }
         self.check_task(name)
 
         get_func = task_default_parameter_map.get(name)
-        return get_func()
+        if name == tt.RT_TDDFT:
+            gs_info = dependent_tasks[0]
+            if gs_info:
+                gs_spacing = gs_info.param.get('spacing')
+            
+            task_default = get_func()
+            task_default.update({'time_step': calc_td_range(gs_spacing)/2})
+            return task_default
+        else:
+            return get_func()
 
     def get_workflow(self, name):
         pass
     
-  
+
 
