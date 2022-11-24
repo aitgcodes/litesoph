6d6c08fa76683c918bb38e93b81b070b387e1828
13sachin <sachins2879@gmail.com>
2022-11-24 13:14:23 +0530

    wip

--- litesoph/common/workflows_data.py
+++ litesoph/common/workflows_data.py
@@ -11,6 +11,7 @@ class WorkflowTypes(str, Enum):
     AVERAGED_SPECTRUM: str = 'averaged_spectrum'
     KOHN_SHAM_DECOMPOSITION: str = 'kohn_sham_decomposition'
     MO_POPULATION_TRACKING: str = 'mo_population_tracking'
+    MASKING: str = 'masking'
 
 
 
@@ -19,12 +20,14 @@ class step:
     id: int
     block_id: int
     task_type: str
+    parameters: Dict[str, str] = field(default_factory=dict)
     env_parameters: Dict[str, str] = field(default_factory=dict)
 
 
 predefined_workflow = Dict[str, Any]
 predefined_workflow = {
     "spectrum": {
+        "name": "Spectrum",
         "blocks": ['Ground State', 'RT TDDFT', 'Compute Spectrum'],
         "steps" : [step(0 ,0, tt.GROUND_STATE),
                     step(1 ,1, tt.RT_TDDFT),
@@ -35,6 +38,7 @@ predefined_workflow = {
     },
 
     "averaged_spectrum": {
+        "name": "Averaged Spectrum",
         "blocks": ['Ground State', 'RT TDDFT', 'Compute Spectrum'],
         "steps" : [step(0 ,0 , tt.GROUND_STATE),
                     step(1 ,1 ,tt.RT_TDDFT, {
@@ -59,6 +63,7 @@ predefined_workflow = {
                             '6' : '3' }
     },
     "kohn_sham_decomposition": {
+        "name" : "Kohn Sham Decomposition",
         "blocks": ['Ground State', 'RT TDDFT', 'Compute Spectrum', 'Compute KSD'],
         "steps" : [step(0 ,0 , tt.GROUND_STATE),
                     step(1 ,1 ,tt.RT_TDDFT,{
@@ -74,6 +79,7 @@ predefined_workflow = {
         
     },
     "mo_population_tracking": {
+        "name": "MO Population Tracking",
         "blocks": ['Ground State', 'RT TDDFT', 'Compute Spectrum', 'Compute MO population'],
         "steps" : [step(0 ,0 , tt.GROUND_STATE),
                     step(1 ,1 ,tt.RT_TDDFT,{
