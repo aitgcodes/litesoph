c84354ace51c59dff127afd5f5f6d0adcdad9a5a
anandsahujnu11 <anandsahujnu11@gmail.com>
2023-03-04 13:14:04 +0530

    removed unnecessary files

--- docs/source/litesoph/common/data_sturcture/__init__.py
+++
@@ -1,6 +0,0 @@
-from litesoph.common.data_sturcture.utils import *
-from litesoph.common.data_sturcture.data_classes import *
-from litesoph.common.data_sturcture.data_types import *
-import uuid
-
-
