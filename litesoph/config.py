807a84ba7464aa083bce448a5a3d000b3e2bcb41
adhtri001 <adheeshtrivedi@gmail.com>
2024-02-11 16:36:43 +0530

    Windows portability initiated

--- litesoph/config.py
+++ litesoph/config.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import pathlib
 from configparser import ConfigParser, NoOptionError, NoSectionError
+import platform
 
 import litesoph
 
@@ -20,7 +21,11 @@ sections = {
 
 def get_path(name):
     print("Checking for {}....".format(name))
-    p = subprocess.run(['which', name], capture_output=True, text=True)
+    if platform.system().lower() == 'windows':
+        command = 'where'
+    else:
+        command = 'which'
+    p = subprocess.run([command, name], capture_output=True, text=True)
     if p.stdout and p.returncode == 0:
         print("Found {} in {}".format(name, p.stdout.split()[0]))
         return p.stdout.split()[0]
