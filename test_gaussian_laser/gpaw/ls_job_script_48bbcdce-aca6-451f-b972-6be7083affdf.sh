48cd68cb1df21fca004fe0b5ed9542f594ad7d0d
ranjan <ranjan20@iiserb.ac.in>
2024-05-15 20:36:42 +0530

    delete excess

--- test_gaussian_laser/gpaw/ls_job_script_48bbcdce-aca6-451f-b972-6be7083affdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ranjan/src/litesoph_iiserb/test_gaussian_laser/gpaw/gpaw/TaskTypes.GROUND_STATE;
-## DO NOT REMOVE LINE BELOW
-touch Start_1a0cb016-7f81-4ff5-b55d-e91dbfb8328f
-/usr/bin/python3 gs.py
--- test_gaussian_laser/workflow_1/ls_job_script_48bbcdce-aca6-451f-b972-6be7083affdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ranjan/src/litesoph_iiserb/test_gaussian_laser/workflow_1/octopus;
-## DO NOT REMOVE LINE BELOW
-touch Start_878e31de-bb8f-4f7f-804f-9cef73768588
-/usr/local/bin/octopus &> TaskTypes.RT_TDDFT/td.log
